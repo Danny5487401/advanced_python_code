47aad04f227814cbd35fdd750336b2e199b714ce
xiaxin <xiaxin5487401@gmail.com>
2021-01-24 20:21:16 +0800

    metaclass_orm

---
+++ charper07_original_class/03_attribute_symbol_n_searchprocess.py
@@ -0,0 +1,80 @@
+# 属性描述符
+"""查找过程：
+如果user 是某个类的实例，那么user.age(以及等价的getattr((user,"age"))
+首先调用__getattibute__,
+如果类定义了__getattr__方法，那么在_getattribute__抛出AttributeError会调用__getatttr__,
+而对于描述符(__get__)的调用，则是发生在__getattribute__内部的，
+user = User(),那么user.age顺序如下：
+1.如果"age"是出现在类User或者其基类的__dict__中，且age 是data descriptor,那么调用其__get__
+2.如果"age"是出现在对象user的__dict__中，那么直接返回obj.__dict__["age"]
+3.如果"age"是出现在类Userd或者其基类的__dict__中:
+    3.1  如果age是non-data descriptor,那么调用其__get__方法
+    3.2  如果不是，调用__dict__["age"]
+4.最后如果User有__getattr__方法，调用__getattr__方法，否则调用AttributeError
+"""
+import numbers
+from datetime import date,datetime
+
+# 类型检查,完成get,set_delete魔法方法
+class IntField:
+    # 数据描述符
+    def __get__(self, instance, owner):
+        return self.value
+
+    def __set__(self, instance, value):
+        if not isinstance(value,numbers.Integral):
+            raise ValueError("int value need")
+        # 保存值
+        if value <0:
+            raise ValueError("POSITIVE value needed")
+        # 如果instance.age 会死循环
+        self.value = value
+
+    def __delete__(self, instance):
+        pass
+
+# 属性描述符
+class NonDataIntFied:
+    # 非数据属性描述符
+    def __get__(self, instance, owner):
+        pass
+
+class User:
+    # 属性描述符对象
+    age = IntField()
+    # age = NonDataIntFied()
+
+
+# 数据库中的表对应类
+# class User:
+#     def __init__(self, name, email, birthday):
+#         self.name = name
+#         self.birthday = birthday
+#         # 不像对外暴露，只是规范而已
+#         self._age = 0
+#         self.email = email
+#
+#     # 获取年龄 方法一
+#     # def get_age(self):
+#     #       return datetime.now().year - self.birthday.year
+#
+#     #方法二 获取年龄
+#     # 属性描述符
+#     @property
+#     def age(self):
+#         return datetime.now().year - self.birthday.year
+#
+#     @age.setter
+#     def age(self,value):
+#         # 检查是否是字符串类型
+#         self._age = value
+
+if __name__ == "__main__":
+    user = User()
+    # user.age = 30
+    user.__dict__["age"] = 33
+    print(user.__dict__)
+    print(user.__dict__["age"])
+    # . 取属性 注意查找顺序
+    # print(user.age)
+    # print(getattr(user,"age"))
---
+++ charper07_original_class/06_orm.py
@@ -0,0 +1,137 @@
+# 需求
+# 类映射数据库中的一张表
+import numbers
+
+
+class Field:
+    pass
+
+
+class IntField(Field):
+    def __init__(self,db_column, min_value=None, max_value=None):
+        self._value = None
+        self.min_value = min_value
+        self.max_value = max_value
+        self.db_column = db_column
+        if min_value is not None:
+            if not isinstance(min_value, numbers.Integral):
+                raise ValueError("int value need")
+            elif min_value < 0:
+                raise ValueError("positive value needed")
+
+        if max_value is not None:
+            if not isinstance(max_value, numbers.Integral):
+                raise ValueError("int value need")
+            elif max_value < 0:
+                raise ValueError("positive value needed")
+
+        if min_value is not None and max_value is not None:
+            if min_value > max_value:
+                raise ValueError("min_value must be smaller than max_value")
+
+    # 数据描述符
+    def __get__(self, instance, owner):
+        return self._value
+
+    def __set__(self, instance, value):
+        if not isinstance(value,numbers.Integral):
+            raise ValueError("int value need")
+        # 保存值
+        if value <0:
+            raise ValueError("POSITIVE value needed")
+        if value < self.min_value or value > self.max_value:
+            raise ValueError("value must be between min_value and max_value")
+        # 如果instance.age 会死循环
+        self._value = value
+
+    def __delete__(self, instance):
+        pass
+
+
+class CharField(Field):
+    def __init__(self,db_column, max_length = None):
+        self._value = None
+        self.db_column = db_column
+        self.max_length = max_length
+        if max_length is None:
+            raise ValueError("max_length needs to be specified")
+
+    def __get__(self, instance, owner):
+        return self._value
+
+    def __set__(self, instance, value):
+        if not isinstance(value, str):
+            raise ValueError("string value needed")
+        if len(value) > self.max_length:
+            raise ValueError("len of value excesses len of max_length")
+        self._value = value
+
+
+class ModelMetaClass(type):
+    def __new__(cls, name, bases, attrs, *args, **kwargs):
+        if name == "BaseModel":
+            return super().__new__(cls, name, bases, attrs, *args, **kwargs)
+        fields = {}
+        for key,value in attrs.items():
+            # 判断是不是表中的列
+            if isinstance(value,Field):
+                fields[key] = value
+        attrs_meta = attrs.get("Meta", None)
+        _meta = {}
+        db_table = name.lower()
+        if attrs_meta is not None:
+            table = getattr(attrs_meta,"db_table", None)
+        if db_table is not None:
+            db_table = table
+        _meta["db_table"] = db_table
+        attrs["_meta"] = _meta
+        attrs["fields"] = fields
+        del attrs["Meta"]
+        return super().__new__(cls, name, bases, attrs, *args, **kwargs)
+
+
+class BaseModel(metaclass=ModelMetaClass):
+    def __init__(self,*args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        return super().__init__()
+
+    def save(self):
+        fields = []
+        values = []
+        for key,value in self.fields.items():
+            db_column = value.db_column
+            if db_column is None:
+                db_column = key.lower
+            fields.append(db_column)
+            value = getattr(self, key)
+            values.append(str(value))
+
+        # sql = 'insert user(name,age) value("danny",30)'
+        # .join 要同种类型
+        sql = 'insert {db_table}({fields}) values({values})' .format(db_table=self._meta["db_table"],
+                                                                     fields="," .join(fields), values="," .join(values))
+        pass
+
+
+# class User(metaclass=ModelMetaClass):
+class User(BaseModel):
+    # # 初始化让基类完成
+    # def __init__(self,name, age):
+    #     pass
+    name = CharField(db_column="name", max_length=10)
+    age = IntField(db_column="age", min_value=1, max_value=100)
+
+    class Meta:
+        db_table = "user"
+
+
+
+if __name__ == "__main__":
+    # 方法一
+    # user = User()
+    # user.name ="danny"
+    # user.age =26
+    # 方法二
+    user = User(name = "danny2",age=11)
+    user.save()
