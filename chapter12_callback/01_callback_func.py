d004044d15c22250c960e6711ea42f6be22bebba
xiaxin <xiaxin5487401@gmail.com>
2021-04-12 17:25:30 +0800

    call_back_func_add_comment

--- chapter12_callback/01_callback_func.py
+++ chapter12_callback/01_callback_func.py
@@ -15,23 +15,34 @@ class Person:
     def __init__(self):
         pass
 
+    # 方法中最终执行回调的逻辑
     def callback(self, data, param=None):
-        print(self.__class__.__name__ + " callback:")
+        print("开始执行类成员回调函数")
+        print("类方法是："+self.__class__.__name__ + " callback:")
         print("data: " + str(data))
         print("param: " + str(param))
+        print("结束执行类成员回调函数")
+        print("\n")
 
+    #类函数中传入普通函数
     def do_call_func(self, func, data, param=None):
-        print(self.__class__.__name__ + " do_call_func:")
+        print("类方法是："+self.__class__.__name__ + " do_call_func:")
         func(data, param)
 
+    # 类函数中传入类成员函数
     def do_call_class_func(self, func, data, obj, param=None):
-        print(self.__class__.__name__ + " do_call_class_func:")
+        print("类方法是："+self.__class__.__name__ + " do_call_class_func:")
         func(obj, data, param)
 
 
-def mycallback(data, param=None):
-    print("data: " + str(data))
-    print("param: " + str(param))
+
+# 普通函数中最终执行回调的逻辑
+def my_callback(data, param=None):
+    print("开始执行普通回调函数")
+    print("普通回调函数中data: " + str(data))
+    print("普通回调函数中param: " + str(param))
+    print("结束执行普通回调函数")
+    print("\n")
 
 
 def do_call_func(func, data, param=None):
@@ -46,16 +57,16 @@ person = Person()
 
 # common func as callback pass to common func
 # 1. 将普通函数传递给普通函数
-do_call_func(mycallback, "danny1", "xxx")
+do_call_func(my_callback, "danny1", "param1")
 
 # common func as callback pass to class func
 # 2.将普通函数传递给类成员函数
-person.do_call_func(mycallback, "danny2")
+person.do_call_func(my_callback, "danny2", "param2")
 
 # class func as callback pass to common func
 # 3. 将类成员函数传递给普通函数
-do_call_classfunc(Person.callback, "danny3", person, None)
+do_call_classfunc(Person.callback, "danny3", person, "param3")
 
 # class func as callback pass to class func
 # 4. 将类成员函数传递给类成员函数
-person.do_call_class_func(Person.callback, "44444444", person, None)
+person.do_call_class_func(Person.callback, "danny4", person, "param4")
